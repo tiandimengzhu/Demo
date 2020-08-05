b5d98d5d9cd5aa476d735bec4f7688598643e422
lihang <lihang@rtmap.com>
2020-08-05 23:27:20 +0800

    init

---
+++ src/main/java/com/basics/learn/javabasics/JavaBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.basics.learn.javabasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBasicsApplication.class, args);
+    }
+
+}
